f3e61106a2827a94e4ecc696be844aef1415415c
Prideland-Okoi <prideland.okoi@gmail.com>
2023-09-07 10:46:46 +0000

    flask endpoints

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import datetime
+import pytz
+
+app = Flask(__name__)
+
+@app.route('/get-info', methods=['GET'])
+def get_info():
+    slack_name = flask.request.args.get('slack_name')
+    track = flask.request.args.get('track')
+
+    tracks = ["vMarketing","design","frontend","mobile", "backend"]
+    if track not in tracks:
+        return jsonify({"error": "Invalid track selection"}), 400
+    current_utc_time = datetime.datetime.utcnow(pytz.utc)
+    utc_offset_hours = current_utc_time.utcoffset().total_seconds()/3600
+    if abc(utc_hour) > 2:
+        return jsonify({"error":"UTC time offset exceeds +/-2 hours"}),400
+    file_url = requests.get('https://api.github.com/repos/Prideland-Okoi/python-flask-endpoints/commits/HEAD').json()['url']
+    source_code_url = 'https://github.com/Prideland-Okoi/python-flask-endpoints'
+    json_response = {
+        'slack_name': slack_name,
+        'day_of_week': datetime.datime.now().strftime("%A"),
+        'current_utc_time': current_utc_time.strftime("%Y-%m-%d %H:%M:%S %Z"),
+        'track': track,
+        'file_url': file_url,
+        'source_code_url': source_code_url,
+        'status_code': 200
+    }
+
+    return jsonify(json_response)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
